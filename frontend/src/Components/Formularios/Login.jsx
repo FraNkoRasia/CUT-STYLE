f2785fbcac75a70f82d0109bf29e16d6e6f6bc42
Josafat Jimenez <josa221001@gmail.com>
2024-10-24 18:41:49 -0600

    fix: login redirection

--- frontend/src/Components/Formularios/Login.jsx
+++ frontend/src/Components/Formularios/Login.jsx
@@ -1,44 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { Link, Navigate } from "react-router-dom";
-
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
-
-import { jwtDecode } from "jwt-decode";
 import Boton from "../Boton/Boton";
 import "../Formularios/Formulario.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-  const [user, setUser] = useState(null);
-  const [message, setMessage] = useState("");
-  const [messageType, setMessageType] = useState("");
-  const [showMessage, setShowMessage] = useState(false);
-
-  useEffect(() => {
-    const token = sessionStorage.getItem("token");
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        setUser(decodedToken);
-      } catch (error) {
-        console.error("Error al decodificar el token:", error);
-        sessionStorage.removeItem("token");
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (showMessage) {
-      const timer = setTimeout(() => {
-        setShowMessage(false);
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [showMessage]);
 
   const handleChange = (e) => {
     setFormData({
@@ -58,46 +31,25 @@ const Login = () => {
         }
       );
 
-      console.log(response);
-
       sessionStorage.setItem("token", response.data.token);
       localStorage.setItem("username", response.data.user.name);
       localStorage.setItem("email", response.data.user.email);
       localStorage.setItem("role", response.data.user.role.name);
 
-      toast.success("Usuario registrado con exito!");
-
-      Navigate("/");
+      toast.success(`Bienvenido ${response.data.user.name}!`);
+      navigate("/");
     } catch (error) {
-      toast.error("Error creando al usuario");
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        toast.error(`Error al iniciar sesión: ${error.response.data.message}`);
+      } else {
+        toast.error("Error al iniciar sesión. Por favor, intenta nuevamente.");
+      }
     }
   };
-
-  // const handleSubmit = async (e) => {
-  //     e.preventDefault();
-  //     try {
-  //         const respuesta = await conexionApi.login({
-  //             email: formData.email,
-  //             password: formData.password
-  //         });
-
-  //         if (respuesta && respuesta.token) {
-  //             const decodedToken = jwtDecode(respuesta.token);
-  //             const userRole = decodedToken.rol;
-
-  //             sessionStorage.setItem('token', respuesta.token);
-  //             sessionStorage.setItem('rol', userRole);
-  //             auth.login(respuesta.token, userRole);
-  //             setUser(decodedToken);
-  //             // Redirigir a /home solo si el inicio de sesión es exitoso
-  //             window.location.href = '/home';
-  //         }
-  //     } catch (error) {
-  //         setMessage(`Error al iniciar sesión: ${error.message}`);
-  //         setMessageType('error');
-  //         setShowMessage(true);
-  //     }
-  // };
 
   return (
     <div className="form-container">
@@ -128,9 +80,8 @@ const Login = () => {
           <Link to="/forgot-password" className="olvidaste">
             ¿Olvidaste tu contraseña?
           </Link>
-          {showMessage && <p className={`mensaje ${messageType}`}>{message}</p>}
         </form>
-        <Toaster />
+        <Toaster reverseOrder={false} />
       </section>
     </div>
   );
--- frontend/src/Components/Formularios/RegisterStylist.jsx
+++ frontend/src/Components/Formularios/RegisterStylist.jsx
@@ -1,99 +1,150 @@
-import React, { useState, useEffect } from 'react';
-import { conexionApi } from '../Modules/conexionApi';
-import Boton from '../Boton/Boton';
-import '../Formularios/Formulario.css';
+import React, { useState, useEffect } from "react";
+import { conexionApi } from "../Modules/conexionApi";
+import Boton from "../Boton/Boton";
+import "../Formularios/Formulario.css";
 
 export default function RegisterBarbershop() {
-    const [formData, setFormData] = useState({
-        name: '',
-        address: '',
-        phone: '',
-        latitude: '',
-        longitude: '',
-        image: null,  // Para almacenar el archivo de imagen
+  const [formData, setFormData] = useState({
+    name: "",
+    address: "",
+    phone: "",
+    latitude: "",
+    longitude: "",
+    image: null, // Para almacenar el archivo de imagen
+  });
+  const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState(""); // 'success' o 'error'
+  const [showMessage, setShowMessage] = useState(false); // Para controlar la visibilidad del mensaje
+
+  const handleChange = (e) => {
+    const { name, value, type, files } = e.target;
+    setFormData({
+      ...formData,
+      [name]: type === "file" ? files[0] : value, // Si es archivo, guarda el archivo; de lo contrario, el valor.
     });
-    const [message, setMessage] = useState('');
-    const [messageType, setMessageType] = useState(''); // 'success' o 'error'
-    const [showMessage, setShowMessage] = useState(false); // Para controlar la visibilidad del mensaje
+  };
 
-    const handleChange = (e) => {
-        const { name, value, type, files } = e.target;
-        setFormData({
-            ...formData,
-            [name]: type === 'file' ? files[0] : value  // Si es archivo, guarda el archivo; de lo contrario, el valor.
-        });
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    // Crear un objeto FormData para manejar la imagen y los otros datos
+    const data = new FormData();
+    data.append("name", formData.name);
+    data.append("address", formData.address);
+    data.append("phone", formData.phone);
+    data.append("latitude", formData.latitude);
+    data.append("longitude", formData.longitude);
+    if (formData.image) {
+      data.append("image", formData.image);
+    }
 
-        // Crear un objeto FormData para manejar la imagen y los otros datos
-        const data = new FormData();
-        data.append('name', formData.name);
-        data.append('address', formData.address);
-        data.append('phone', formData.phone);
-        data.append('latitude', formData.latitude);
-        data.append('longitude', formData.longitude);
-        if (formData.image) {
-            data.append('image', formData.image);
-        }
+    try {
+      const respuesta = await conexionApi.registro(data); // Envío con FormData
+      if (respuesta) {
+        setMessage("Registro exitoso");
+        setMessageType("success");
+        setShowMessage(true);
+        setTimeout(() => {
+          window.location.href = "/login";
+        }, 5000); // Redirige después de 5 segundos para permitir que el mensaje se vea
+      }
+    } catch (error) {
+      setMessage(`Error al registrar: ${error.message}`);
+      setMessageType("error");
+      setShowMessage(true);
+    }
+  };
 
-        try {
-            const respuesta = await conexionApi.registro(data);  // Envío con FormData
-            if (respuesta) {
-                setMessage('Registro exitoso');
-                setMessageType('success');
-                setShowMessage(true);
-                setTimeout(() => {
-                    window.location.href = '/login';
-                }, 5000); // Redirige después de 5 segundos para permitir que el mensaje se vea
-            }
-        } catch (error) {
-            setMessage(`Error al registrar: ${error.message}`);
-            setMessageType('error');
-            setShowMessage(true);
-        }
-    };
+  useEffect(() => {
+    if (showMessage) {
+      const timer = setTimeout(() => {
+        setShowMessage(false);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [showMessage]);
 
-    useEffect(() => {
-        if (showMessage) {
-            const timer = setTimeout(() => {
-                setShowMessage(false);
-            }, 5000);
-            return () => clearTimeout(timer);
-        }
-    }, [showMessage]);
+  return (
+    <div className="form-container">
+      <section className="section-formulario">
+        <form
+          className="Formulario"
+          onSubmit={handleSubmit}
+          encType="multipart/form-data"
+        >
+          <h1>REGISTER BARBERSHOP</h1>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+          />
 
-    return (
-        <div className="form-container">
-            <section className='section-formulario'>
-                <form className='Formulario' onSubmit={handleSubmit} encType="multipart/form-data">
-                    <h1>REGISTER BARBERSHOP</h1>
-                    <label htmlFor="name">Name</label>
-                    <input id="name" name="name" type="text" placeholder="Name" value={formData.name} onChange={handleChange} required />
+          <label htmlFor="address">Address</label>
+          <input
+            id="address"
+            name="address"
+            type="text"
+            placeholder="Address"
+            value={formData.address}
+            onChange={handleChange}
+            required
+          />
 
-                    <label htmlFor="address">Address</label>
-                    <input id="address" name="address" type="text" placeholder="Address" value={formData.address} onChange={handleChange} required />
+          <label htmlFor="phone">Phone</label>
+          <input
+            id="phone"
+            name="phone"
+            type="tel"
+            placeholder="Phone Number"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
 
-                    <label htmlFor="phone">Phone</label>
-                    <input id="phone" name="phone" type="tel" placeholder="Phone Number" value={formData.phone} onChange={handleChange} required />
+          <label htmlFor="latitude">Latitude</label>
+          <input
+            id="latitude"
+            name="latitude"
+            type="number"
+            placeholder="Latitude"
+            value={formData.latitude}
+            onChange={handleChange}
+            required
+          />
 
-                    <label htmlFor="latitude">Latitude</label>
-                    <input id="latitude" name="latitude" type="number" placeholder="Latitude" value={formData.latitude} onChange={handleChange} required />
+          <label htmlFor="longitude">Longitude</label>
+          <input
+            id="longitude"
+            name="longitude"
+            type="number"
+            placeholder="Longitude"
+            value={formData.longitude}
+            onChange={handleChange}
+            required
+          />
 
-                    <label htmlFor="longitude">Longitude</label>
-                    <input id="longitude" name="longitude" type="number" placeholder="Longitude" value={formData.longitude} onChange={handleChange} required />
+          <label htmlFor="image">Image</label>
+          <input
+            id="image"
+            name="image"
+            type="file"
+            accept="image/*"
+            onChange={handleChange}
+            required
+          />
 
-                    <label htmlFor="image">Image</label>
-                    <input id="image" name="image" type="file" accept="image/*" onChange={handleChange} required />
+          <p style={{ color: "white" }}>All fields are required</p>
 
-                    <p style={{ color: 'white' }}>All fields are required</p>
+          <Boton texto="Register Barbershop" className="boton " />
 
-                    <Boton texto="Register Barbershop" className="boton " />
-
-                    {showMessage && <p className={`mensaje ${messageType}`}>{message}</p>}
-                </form>
-            </section>
-        </div>
-    );
-};
+          {showMessage && <p className={`mensaje ${messageType}`}>{message}</p>}
+        </form>
+      </section>
+    </div>
+  );
+}
