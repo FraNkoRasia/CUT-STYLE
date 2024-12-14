33871099631f50a8864d4fc2a239e77d478e6c96
FraNko <rasiafranko@gmail.com>
2024-12-14 09:40:09 -0300

    cambios

--- frontend/src/Components/AdminDashboard/ListBarber.jsx
+++ frontend/src/Components/AdminDashboard/ListBarber.jsx
@@ -111,3 +111,227 @@ export default function ListBarber() {
         </div>
     );
 }
+
+// CREAR EL CRUD DE BARBEROS EN EL BACKEND
+
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import '../BarberShop/BarberShop.css'; // Reutilizando estilos
+// import '../AdminDashboard/List.css';
+// import toast, { Toaster } from "react-hot-toast";
+
+// export default function ListBarbers() {
+//     const [isEditing, setIsEditing] = useState(false);
+//     const [currentBarber, setCurrentBarber] = useState(null);
+//     const [barbers, setBarbers] = useState([]);
+
+//     const token = sessionStorage.getItem("token");
+
+//     useEffect(() => {
+//         const fetchBarbers = async () => {
+//             if (!token) {
+//                 toast.error("No se encontró el token. Por favor, inicia sesión.");
+//                 return;
+//             }
+
+//             try {
+//                 const response = await axios.get(
+//                     `${import.meta.env.VITE_API_URL}/api/v1/barbers`,
+//                     {
+//                         headers: {
+//                             Authorization: `Bearer ${token}`,
+//                         },
+//                     }
+//                 );
+//                 setBarbers(response.data);
+//             } catch (error) {
+//                 console.log(error);
+//                 toast.error("Error al buscar barberos. Por favor, intenta nuevamente.");
+//             }
+//         };
+//         fetchBarbers();
+//     }, [token]);
+
+//     const handleEdit = (barber) => {
+//         setCurrentBarber({ ...barber }); // Copia del barbero actual
+//         setIsEditing(true);
+//     };
+
+//     const handleChange = (e) => {
+//         const { name, value } = e.target;
+//         setCurrentBarber((prevBarber) => ({ ...prevBarber, [name]: value }));
+//     };
+
+//     const handleSave = async () => {
+//         if (!currentBarber) return;
+
+//         const updatedData = {
+//             name: currentBarber.name,
+//             email: currentBarber.email,
+//             phone: currentBarber.phone,
+//             lastname: currentBarber.lastname,
+//             roleId: currentBarber.roleId, // Usando roleId
+//             ...(currentBarber.password && { password: currentBarber.password }), // Solo enviar si hay un cambio de contraseña
+//         };
+
+//         try {
+//             const response = await axios.put(
+//                 `${import.meta.env.VITE_API_URL}/api/v1/barbers/${currentBarber.id}`,
+//                 updatedData,
+//                 {
+//                     headers: {
+//                         Authorization: `Bearer ${token}`,
+//                     },
+//                 }
+//             );
+
+//             // Actualiza el estado de barbers con los datos actualizados
+//             setBarbers((prevBarbers) =>
+//                 prevBarbers.map((barber) =>
+//                     barber.id === response.data.id
+//                         ? { ...barber, ...response.data, role: { name: getRoleName(response.data.roleId) } }
+//                         : barber
+//                 )
+//             );
+
+//             setIsEditing(false);
+//             toast.success("Barbero actualizado correctamente.");
+//         } catch (error) {
+//             console.error(error);
+//             toast.error("Error al actualizar barbero. Por favor, intenta nuevamente.");
+//         }
+//     };
+
+//     const handleDelete = async (id) => {
+//         try {
+//             await axios.delete(
+//                 `${import.meta.env.VITE_API_URL}/api/v1/barbers/${id}`,
+//                 {
+//                     headers: {
+//                         Authorization: `Bearer ${token}`,
+//                     },
+//                 }
+//             );
+//             setBarbers(barbers.filter((barber) => barber.id !== id));
+//             toast.success("Barbero eliminado exitosamente.");
+//         } catch (error) {
+//             console.log(error);
+//             toast.error("Error al eliminar barbero. Por favor, intenta nuevamente.");
+//         }
+//     };
+
+//     const getRoleName = (roleId) => {
+//         const roles = {
+//             1: "ADMIN",
+//             2: "USER",
+//             3: "BARBER",
+//         };
+//         return roles[roleId] || "N/A";
+//     };
+
+//     return (
+//         <div className="turno-container">
+//             <h2>BARBER LIST</h2>
+//             <div className="table-container">
+//                 <div className="table-scroll-container">
+//                     <table>
+//                         <thead>
+//                             <tr>
+//                                 <th>Name</th>
+//                                 <th>Email</th>
+//                                 <th>Phone</th>
+//                                 <th>Role</th>
+//                                 <th>Edit</th>
+//                                 <th>Delete</th>
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//                             {barbers.map((barber) => (
+//                                 <tr key={barber.id}>
+//                                     <td>{barber.name}</td>
+//                                     <td>{barber.email}</td>
+//                                     <td>{barber.phone}</td>
+//                                     <td>{barber.role?.name || "N/A"}</td>
+//                                     <td>
+//                                         <button className="boton custom-class manual-padding" onClick={() => handleEdit(barber)}>Edit</button>
+//                                     </td>
+//                                     <td>
+//                                         <button className="boton custom-class manual-padding" onClick={() => handleDelete(barber.id)}>Delete</button>
+//                                     </td>
+//                                 </tr>
+//                             ))}
+//                         </tbody>
+//                     </table>
+//                 </div>
+//             </div>
+
+//             {isEditing && currentBarber && (
+//                 <div className="modal-over">
+//                     <div className="modal">
+//                         <button className="close-modal" onClick={() => setIsEditing(false)}>✖</button>
+//                         <h3 className='titleModal-Listuser'>EDIT BARBER</h3>
+//                         <form>
+//                             <div className="modalForm-row">
+//                                 <label>
+//                                     Name:
+//                                     <input
+//                                         type="text"
+//                                         name="name"
+//                                         value={currentBarber.name}
+//                                         onChange={handleChange}
+//                                     />
+//                                 </label>
+//                             </div>
+//                             <div className="modalForm-row">
+//                                 <label>
+//                                     Email:
+//                                     <input
+//                                         type="email"
+//                                         name="email"
+//                                         value={currentBarber.email}
+//                                         onChange={handleChange}
+//                                     />
+//                                 </label>
+//                             </div>
+//                             <div className="modalForm-row">
+//                                 <label>
+//                                     Phone:
+//                                     <input
+//                                         type="text"
+//                                         name="phone"
+//                                         value={currentBarber.phone}
+//                                         onChange={handleChange}
+//                                     />
+//                                 </label>
+//                             </div>
+//                             <div className="modalForm-row">
+//                                 <label>
+//                                     Role:
+//                                     <select
+//                                         name="role"
+//                                         value={currentBarber.role?.name || ""}
+//                                         onChange={(e) =>
+//                                             setCurrentBarber((prevBarber) => ({
+//                                                 ...prevBarber,
+//                                                 role: { ...prevBarber.role, name: e.target.value },
+//                                             }))
+//                                         }
+//                                     >
+//                                         <option value="ADMIN">ADMIN</option>
+//                                         <option value="USER">USER</option>
+//                                         <option value="BARBER">BARBER</option>
+//                                     </select>
+//                                 </label>
+//                             </div>
+//                             <div className="modal-buttons btnModal-userlist">
+//                                 <button type="button" className="boton custom-class" onClick={handleSave}>Save</button>
+//                                 <button type="button" className="boton custom-class" onClick={() => setIsEditing(false)}>Cancel</button>
+//                             </div>
+//                         </form>
+//                     </div>
+//                 </div>
+//             )}
+//             <Toaster reverseOrder={false} position="top-center" containerStyle={{ marginTop: "90px" }} />
+//         </div>
+//     );
+// }
--- frontend/src/Components/AdminDashboard/ListUsers.jsx
+++ frontend/src/Components/AdminDashboard/ListUsers.jsx
@@ -1,35 +1,120 @@
-import React, { useState } from 'react';
-import '../BarberShop/BarberShop.css'; // Importamos el CSS de BarberShop para reutilizar estilos
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import '../BarberShop/BarberShop.css'; // Reutilizando estilos
 import '../AdminDashboard/List.css';
+import toast, { Toaster } from "react-hot-toast";
 
 export default function ListUsers() {
-    const [datos, setDatos] = useState([
-        { id: 1, name: 'Juan Pérez', email: 'Barbería@gmail.com', phone: '154111555', role: 'USER' },
-        { id: 2, name: 'Facundo Pérez', email: 'Styl@gmail.com', phone: '155222000', role: 'USER' }
-    ]);
-
     const [isEditing, setIsEditing] = useState(false);
     const [currentUser, setCurrentUser] = useState(null);
+    const [users, setUsers] = useState([]);
+
+    const token = sessionStorage.getItem("token");
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            if (!token) {
+                toast.error("No se encontró el token. Por favor, inicia sesión.");
+                return;
+            }
+
+            try {
+                const response = await axios.get(
+                    `${import.meta.env.VITE_API_URL}/api/v1/users`,
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                // Se espera que los datos incluyan el rol como un objeto con 'name'
+                setUsers(response.data);
+            } catch (error) {
+                console.log(error);
+                toast.error("Error al buscar usuarios. Por favor, intenta nuevamente.");
+            }
+        };
+        fetchUsers();
+    }, [token]);
 
     const handleEdit = (user) => {
-        setCurrentUser(user);
+        setCurrentUser({ ...user }); // Copia del usuario actual
         setIsEditing(true);
-    };
-
-    const handleDelete = (id) => {
-        setDatos(datos.filter(user => user.id !== id));
-    };
-
-    const handleSave = () => {
-        setDatos(datos.map(user =>
-            user.id === currentUser.id ? currentUser : user
-        ));
-        setIsEditing(false);
     };
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setCurrentUser({ ...currentUser, [name]: value });
+        setCurrentUser((prevUser) => ({ ...prevUser, [name]: value }));
+    };
+
+    const handleSave = async () => {
+        if (!currentUser) return;
+    
+        const updatedData = {
+            name: currentUser.name,
+            email: currentUser.email,
+            phone: currentUser.phone,
+            lastname: currentUser.lastname,
+            roleId: currentUser.roleId, // Usando roleId
+            ...(currentUser.password && { password: currentUser.password }), // Solo enviar si hay un cambio de contraseña
+        };
+    
+        try {
+            const response = await axios.put(
+                `${import.meta.env.VITE_API_URL}/api/v1/users/${currentUser.id}`,
+                updatedData,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+    
+            // Actualiza el estado de users con los datos actualizados
+            setUsers((prevUsers) =>
+                prevUsers.map((user) =>
+                    user.id === response.data.id
+                        ? { ...user, ...response.data, role: { name: getRoleName(response.data.roleId) } }
+                        : user
+                )
+            );
+    
+            setIsEditing(false);
+            toast.success("Usuario actualizado correctamente.");
+        } catch (error) {
+            console.error(error);
+            toast.error("Error al actualizar usuario. Por favor, intenta nuevamente.");
+        }
+    };
+    
+    // Función para obtener el nombre del rol según el ID
+    const getRoleName = (roleId) => {
+        const roles = {
+            1: "ADMIN",
+            2: "USER",
+            3: "BARBER",
+        };
+        return roles[roleId] || "N/A";
+    };
+    
+
+
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(
+                `${import.meta.env.VITE_API_URL}/api/v1/users/${id}`,
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            setUsers(users.filter((user) => user.id !== id));
+            toast.success("Usuario eliminado exitosamente.");
+        } catch (error) {
+            console.log(error);
+            toast.error("Error al eliminar usuario. Por favor, intenta nuevamente.");
+        }
     };
 
     return (
@@ -49,17 +134,17 @@ export default function ListUsers() {
                             </tr>
                         </thead>
                         <tbody>
-                            {datos.map((barber) => (
-                                <tr key={barber.id}>
-                                    <td>{barber.name}</td>
-                                    <td>{barber.email}</td>
-                                    <td>{barber.phone}</td>
-                                    <td>{barber.role}</td>
+                            {users.map((user) => (
+                                <tr key={user.id}>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phone}</td>
+                                    <td>{user.role?.name || "N/A"}</td> {/* Mostrar el nombre del rol */}
                                     <td>
-                                        <button className="boton custom-class manual-padding" onClick={() => handleEdit(barber)}>Edit</button>
+                                        <button className="boton custom-class manual-padding" onClick={() => handleEdit(user)}>Edit</button>
                                     </td>
                                     <td>
-                                        <button className="boton custom-class manual-padding" onClick={() => handleDelete(barber.id)}>Delete</button>
+                                        <button className="boton custom-class manual-padding" onClick={() => handleDelete(user.id)}>Delete</button>
                                     </td>
                                 </tr>
                             ))}
@@ -68,14 +153,23 @@ export default function ListUsers() {
                 </div>
             </div>
 
-
-            {/* Modal de edición */}
-            {isEditing && (
+            {isEditing && currentUser && (
                 <div className="modal-over">
                     <div className="modal">
                         <button className="close-modal" onClick={() => setIsEditing(false)}>✖</button>
                         <h3 className='titleModal-Listuser'>EDIT USER</h3>
                         <form>
+                            <div className="modalForm-row">
+                                <label>
+                                    Name:
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        value={currentUser.name}
+                                        onChange={handleChange}
+                                    />
+                                </label>
+                            </div>
                             <div className="modalForm-row">
                                 <label>
                                     Email:
@@ -89,19 +183,37 @@ export default function ListUsers() {
                             </div>
                             <div className="modalForm-row">
                                 <label>
-                                    Role:
-                                    <select
-                                        name="role"
-                                        value={currentUser.role}
+                                    Phone:
+                                    <input
+                                        type="text"
+                                        name="phone"
+                                        value={currentUser.phone}
                                         onChange={handleChange}
-                                        required
-                                    >
-                                        <option value="USER">USER</option>
-                                        <option value="BARBER">BARBER</option>
-                                        <option value="ADMIN">ADMIN</option>
-                                    </select>
+                                    />
                                 </label>
                             </div>
+                            <div className="modalForm-row">
+                                <label>
+                                    Role:
+                                    <select
+                                        name="roleId"
+                                        value={currentUser.roleId || ""}
+                                        onChange={(e) =>
+                                            setCurrentUser((prevUser) => ({
+                                                ...prevUser,
+                                                roleId: parseInt(e.target.value, 10),
+                                            }))
+                                        }
+                                    >
+                                        <option value="1">ADMIN</option>
+                                        <option value="2">USER</option>
+                                        <option value="3">BARBER</option>
+                                    </select>
+
+                                </label>
+                            </div>
+
+
                             <div className="modal-buttons btnModal-userlist">
                                 <button type="button" className="boton custom-class" onClick={handleSave}>Save</button>
                                 <button type="button" className="boton custom-class" onClick={() => setIsEditing(false)}>Cancel</button>
@@ -110,6 +222,7 @@ export default function ListUsers() {
                     </div>
                 </div>
             )}
+            <Toaster reverseOrder={false} position="top-center" containerStyle={{ marginTop: "90px" }} />
         </div>
     );
 }
